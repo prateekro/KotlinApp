b46b4a9345ce9e12ba2980e3c5338ab1a498d1fc
Meenakshi <meenakshi.chouhan@gyrix.in>
2017-04-19 15:05:24 +0530

    Revert "Test 1"
    This reverts commit b3e7325ab3b3df7c72928b6561db6892d976682a.

--- KedditBySteps/app/src/main/java/co/prateek/kedditbysteps/MainActivity.java
+++ KedditBySteps/app/src/main/java/co/prateek/kedditbysteps/MainActivity.java
@@ -2,7 +2,6 @@ package co.prateek.kedditbysteps;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,11 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Toast.makeText(this, "Loading Complete", Toast.LENGTH_SHORT).show();
     }
 }
