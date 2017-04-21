c8e81201cfd4a2a79af0187e77870b896784d794
Meenakshi <meenakshi.chouhan@gyrix.in>
2017-04-21 14:40:43 +0530

    Kotlin - struggling with lateinit

--- KedditBySteps/app/src/main/java/co/prateek/gyrix/MainActivity.kt
+++ KedditBySteps/app/src/main/java/co/prateek/gyrix/MainActivity.kt
@@ -2,20 +2,22 @@ package co.prateek.gyrix
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Message
 import android.support.v7.widget.Toolbar
 import android.view.KeyEvent
 import android.widget.Toast
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.view.KeyEvent.KEYCODE_BACK
-
-
+import android.webkit.WebChromeClient
+import android.webkit.WebResourceRequest
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
 
     //var mywebview : WebView = null  //as WebView
-    var mywebview : WebView? = null
+    lateint var mywebview : WebView //? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,16 +32,16 @@ class MainActivity : AppCompatActivity() {
         mywebview = findViewById(R.id.webViewGyrix) as WebView
 
         (mywebview as WebView).setWebViewClient(object : WebViewClient() {
-            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
-                view?.loadUrl(url);
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                (view as WebView).loadUrl(request?.getUrl().toString());
                 return true
             }
         })
 
-        (mywebview as WebView).loadUrl("http://www.gyrix.com.au/")
-        (mywebview as WebView).settings.displayZoomControls.and(true)
-        Toast.makeText(this, "Loading Completed", Toast.LENGTH_SHORT).show()
+        (mywebview as WebView).getSettings().setJavaScriptEnabled(true)
+        (mywebview as WebView).getSettings().setSupportZoom(true)
 
+        (mywebview as WebView).loadUrl("http://www.gyrix.com.au/")
     }
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
